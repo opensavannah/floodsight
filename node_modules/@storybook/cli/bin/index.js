916c5c8453e06a58948b91ac6c4a14a478173cb1
Carl V. Lewis <carl@carlvlewis.net>
2019-06-20 22:28:04 -0400

    Revert "ADD NODE MODULES"
    This reverts commit 918892cf51d2cac10cd7c2702ff3196e2663dd81.

--- node_modules/@storybook/cli/bin/index.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-require('babel-register')({
-  ignore: /node_modules\/(?!@storybook\/cli)/,
-});
-require('./generate');
--- node_modules/@storybook/cli/lib/has_yarn.js
+++
@@ -1,6 +0,0 @@
-import { sync as spawnSync } from 'cross-spawn';
-
-export default function hasYarn() {
-  const result = spawnSync('yarn', ['--version'], { silent: true });
-  return result.status === 0;
-}
